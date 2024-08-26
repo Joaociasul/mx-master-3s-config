e3ecef2004520a61e1ec8e75fec5b2526c1d7c04
João Royer <joaociasul@hotmail.com>
2024-08-26 08:05:18 -0300

    fix: install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #https://pwr-solaar.github.io/Solaar/rules/#process-focused
 python3 -m pip install pynput
-sudo apt-get install libx11-6 libx11-dev
+sudo apt-get update && sudo apt-get install -y libx11-6 libx11-dev
 sudo setfacl -m u:${USER}:rw /dev/uinput
